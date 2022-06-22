b2919ddef32f55c6a21ab71c68f9ee945fb90400
rivi <javivarro@msn.com>
2022-06-22 21:58:22 +0200

    NewUser component HTML & CSS first design finished

--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -1,8 +1,16 @@
 
-
 const NewUser = () => {
   return (
-    <div>NewUser</div>
+    <div>
+      <h2>New User</h2>
+      <form className="form">
+        <label className="label-form" htmlFor="firstName" >First name: </label>
+        <input className="item-form" type="text" id="firstName" name="firstName" placeholder="First name"></input>
+        <label className="label-form" htmlFor="lastName">Last name:  </label>
+        <input className="item-form" type="text" id="lastName" name="lastName" placeholder="Last name"></input>
+        <input className="button-form" type="submit" value="Save"></input>
+      </form>
+    </div>
   )
 }
 
