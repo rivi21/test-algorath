560663413b9379c8525007655f370118db50cd8e
rivi <javivarro@msn.com>
2022-06-23 14:20:22 +0200

    component UserConnections html css and js  finished

--- client/src/Settings.js
+++ client/src/Settings.js
@@ -1,7 +1,11 @@
 const DUMMY_URL = "https://reqres.in/api/users?page=2";
+const URL_GET = "";
+const URL_GET_CONNECTIONS = "";
 const URL_POST = "";
 
 export {
     DUMMY_URL,
+    URL_GET,
+    URL_GET_CONNECTIONS,
     URL_POST
 }
--- client/src/components/UserConnections.js
+++ client/src/components/UserConnections.js
@@ -1,7 +1,31 @@
+import { useState } from "react"
+import { DUMMY_URL } from "../Settings"
 
 const UserConnections = () => {
+  const [connected, setConnected] = useState([])
+
+  const searchConnections = () => {
+    fetch(DUMMY_URL) // En real hace falta parámetro con el nombre del usuario
+      .then(response => response.json())
+      .then(data => setConnected(data.data))
+  }
+
   return (
-    <div>UserConnections</div>
+    <div>
+      <h2>User connections</h2>
+      <div className="user-connections-form">
+        <label htmlFor="user"><p>Search connections by user:</p></label>
+        <input className="user-connections-item"type="text" placeholder="user name" />
+        <input className="user-connections-button"type="button" value="Search" onClick={searchConnections} />
+      </div>
+      <div>
+        <ul classNAme="user-connections-list">
+          {connected.map(user => {
+            return <li className="connections-item" key={user.id}>{user.first_name} {user.last_name}</li>
+          })}
+        </ul>
+      </div>
+    </div>
   )
 }
 
