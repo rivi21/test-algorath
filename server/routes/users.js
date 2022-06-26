46bf3212929960c3b69a62e35d80b701197c2229
rivi <javivarro@msn.com>
2022-06-26 20:48:00 +0200

    modified new user route

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,9 +4,10 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 
-//conexión con la base de datos
+//connection with the dataBase
 const { connection } = require("../config.db");
 
+//Route for All Users
 app.get("/users", (req, res) => {
     connection.query("SELECT * FROM users",
         (error, results) => {
@@ -16,13 +17,24 @@ app.get("/users", (req, res) => {
             res.status(200).json(results);
         });
 });
+//Route for connected users to one User
+app.get("/users/:id", (req, res) => {
+    const id = req.params.id;
+    connection.query("SELECT * FROM users JOIN connections ON users.id = users_id JOIN connected ON connected.id = connected_id WHERE users.id = ?;",
+        [id],
+        (error, results) => {
+            if (error) {
+                throw error;
+            }
+            res.status(200).json(results);
+        });
+});
 
-
+//Route for New User
 app.post("/users", (req, res) => {
     const body = req.body;
-
     if (!body.first_name || !body.last_name) {
-        res.status(400).json({ ok: false, message: "complete name is required" })
+        res.status(400).json({ ok: false, message: "first and last names are required" })
     } else {
         connection.query("INSERT INTO users(first_name,last_name) VALUES (?,?)",
             [body.first_name, body.last_name],
@@ -30,23 +42,26 @@ app.post("/users", (req, res) => {
                 if (error) {
                     throw error;
                 }
-                res.status(201).json({user: body});
+                res.status(201).json({
+                    id: results.insertId,
+                    first_name: body.first_name,
+                    last_name: body.last_name
+                });
             });
     }
 
 });
-
+//Route for New Connection
 app.post("/connections", (req, res) => {
     const body = req.body;
-    console.log(body)
-    connection.query("INSERT INTO connections(users_id, connected_id) VALUES(?,?)", 
-    [body.userId, body.connectedId],
-    (error, results) => {
-        if(error) {
-            throw error;
-        }           
-        res.status(201).json(results);       
-    });
+    connection.query("INSERT INTO connections(users_id, connected_id) VALUES(?,?)",
+        [body.userId, body.connectedId],
+        (error, results) => {
+            if (error) {
+                throw error;
+            }
+            res.status(201).json(results);
+        });
 });
 
 module.exports = app;
